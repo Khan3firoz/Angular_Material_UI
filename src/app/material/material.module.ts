eb90bd7cd98824c1afc2f0b98634a149e4db82d1
Firoz <khan3firoz@gmail.com>
2021-03-09 15:37:13 +0530

    Add button toggle

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 
 
 const materialComponent = [
   MatButtonModule,
-  MatDividerModule
+  MatDividerModule,
+  MatButtonToggleModule
 ]
 
 @NgModule({
